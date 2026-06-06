ff0fe9645accbcbbe0e35c33545f09e5933f55d7
Danil Farafontov <danil.farafontov@office.e2e4gu.ru>
2026-06-07 00:28:24 +0700

    Added hide button to UI Settings bar Added UI Settings button to the left side Added UI Settings screen with show/hide UI Settings Bar toggle

--- src/player/app/store.ts
+++ src/player/app/store.ts
@@ -64,6 +64,31 @@ const migrations = {
         ]
       }
     };
+  },
+  4: (state: any) => {
+    return {
+      ...state,
+      uisettings: {
+        byName: {
+          displayuisettingsbar: { name: 'displayuisettingsbar', value: "true"},
+          gridmode: { name: 'gridmode', value: 'normal'},
+          xscolumnsnumber: { name: 'xscolumnsnumber', value: '6'},
+          smcolumnsnumber: { name: 'smcolumnsnumber', value: '4'},
+          mdcolumnsnumber: { name: 'mdcolumnsnumber', value: '3'},
+          listitemheight: { name: 'listitemheight', value: '200px'},
+          containerwidth: { name: 'containerwidth', value: 'fixed'}
+        },
+        allsettings: [
+          { name: 'displayuisettingsbar', value: "true"},
+          { name: 'gridmode', value: 'normal'},
+          { name: 'xscolumnsnumber', value: '6'},
+          { name: 'smcolumnsnumber', value: '4'},
+          { name: 'mdcolumnsnumber', value: '3'},
+          { name: 'listitemheight', value: '200px'},
+          { name: 'containerwidth', value: 'fixed'}
+        ]
+      }
+    };
   }
 };
 
@@ -88,7 +113,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: "player",
-  version: 3,
+  version: 4,
   storage,
   whitelist: ["playlists", "soundboards", "settings", "uisettings"],
   migrate: createMigrate(migrations, { debug: false }),
